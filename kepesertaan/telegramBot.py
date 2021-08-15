952241d697d62e267cc71af0cc4f7c18d3ea1fd7
s4-hub <syafii.newbie@gmail.com>
2021-08-15 09:53:22 +0700

    Fixing bot script

--- kepesertaan/telegramBot.py
+++ kepesertaan/telegramBot.py
@@ -36,8 +36,8 @@ def newRegister(message):
         username = texts[2]
         password1 = texts[3]
         password2 = texts[4]
-        qs = ExtendUser.objects.filter(username=username, id_telegram=message.chat.id).exists()
-        if qs:
+        qs = ExtendUser.objects.filter(username=username, id_telegram=message.chat.id)
+        if qs.exists() or qs[0].id_telegram == message.chat.id:
             bot.send_message(user, "Username anda sudah pernah terdaftar")
         else:
             query = """
