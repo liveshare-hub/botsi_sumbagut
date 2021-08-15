0261474e3f418cabed05bd4a609beecef5aecf77
s4-hub <syafii.newbie@gmail.com>
2021-08-15 08:13:57 +0700

    db router fixing

--- kepesertaan/dbrouters.py
+++ kepesertaan/dbrouters.py
@@ -1,4 +1,5 @@
 from .models import TargetRealisasi, JenisUraian, Uraian
+from accounts.models import m_jabatan, m_bidang, kode_kantor, ExtendUser
 from detil_mkro.models import DetilMkro
 
 class KepesertaanDBRouter:
@@ -12,6 +13,12 @@ class KepesertaanDBRouter:
     def db_for_write(self, model, **hints):
         if (model == TargetRealisasi) or (model == JenisUraian) or (model == Uraian):
             return 'default'
-        elif (model == DetilMkro):
-            return 'primary'
+
+        return None
+
+    def allow_migrate(self, model, **hints):
+        if (model == TargetRealisasi) or (model == JenisUraian) or (model == Uraian)\
+            or (model == m_bidang) or (model == m_jabatan) or (model == kode_kantor)\
+                or (model == ExtendUser)
+            return 'default'
         return None
